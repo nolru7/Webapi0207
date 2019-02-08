df5bdca361971d3cd758cf9baf050dda91fd0040
석훈 이 <nolru7@naver.com>
2019-02-08 13:52:26 +0900

    데이터테스트

---
+++ Solution/WebApplication/Controllers/DataController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MySql.Data.MySqlClient;
+using WebApplication.Modules;
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebApplication.Controllers
+{
+    
+
+    public class DataController : Controller
+    {
+        private ArrayList list;
+        [Route("api/Select")]
+        [HttpGet]
+        public ArrayList Select()
+        {
+            list = new ArrayList();
+
+            Database db = new Database();
+            MySqlDataReader sdr = db.Reader("select * from Notice");
+
+            while (sdr.Read())
+            {
+                Hashtable ht = new Hashtable();
+                //string[] arr = new string[sdr.FieldCount];
+                for (int i = 0; i < sdr.FieldCount; i++)
+                {
+                    ht.Add(sdr.GetName(i).ToString(), sdr.GetValue(i).ToString());
+                    //arr[i] = sdr.GetValue(i).ToString();
+                }
+
+                list.Add(ht);
+            }
+
+            return list;
+        }
+
+        
+    }
+}
---
+++ Solution/WebApplication/Modules/Database.cs
@@ -0,0 +1,114 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication.Modules
+{
+    public class Database
+    {
+        private MySqlConnection conn;
+        private bool status;
+
+        public Database()
+        {
+            status = Connection();
+        }
+
+        private bool Connection()
+        {
+            string host = "192.168.3.149";
+            string user = "root";
+            string password = "1234";
+            string db = "gudi";
+            //string port = "";
+            string connStr = string.Format("server={0};uid={1};password={2};database={3};port={4}", host, user, password,db/*,port*/);
+            MySqlConnection conn = new MySqlConnection(connStr);
+
+            try
+            {
+                conn.Open();
+                this.conn = conn;
+                return true;
+            }
+            catch
+            {
+                conn.Close();
+                this.conn = null;
+                return false;
+            }
+        }
+
+        public bool ConnectionClose()
+        {
+            try
+            {
+                conn.Close();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public bool NonQuery(string sql)
+        {
+            try
+            {
+                if (status)
+                {
+                    MySqlCommand comm = new MySqlCommand(sql, conn);
+                    comm.ExecuteNonQuery();
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public MySqlDataReader Reader(string sql)
+        {
+            try
+            {
+                if (status)
+                {
+                    MySqlCommand comm = new MySqlCommand(sql, conn);
+                    
+                    return comm.ExecuteReader();
+                }
+                else
+                {
+                    
+                    return null;
+                }
+            }
+            catch
+            {
+                
+                return null;
+            }
+
+        }
+
+        public MySqlDataReader Test(string sql)
+        {
+            MySqlCommand comm = new MySqlCommand(sql, conn);
+            return comm.ExecuteReader();
+            //Console.WriteLine(sql);
+        }
+        public void ReaderClose(MySqlDataReader reader)
+        {
+            reader.Close();
+        }
+    }
+}
